7fb9c016be398282fcc5cda3cee988bdf20b76cb
Justin Breneman <justin.breneman@yahoo.com>
2020-05-09 02:39:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -29,5 +29,9 @@ def kesha_maker(array)
     array2 << word.split('')
   end
   array2[0][2] = '$'
-  array2
+  array2[1][2] = '$'
+  array2[2][2] = '$'
+  array2.each_with_index do |word, index|
+    array[index] = word.join(',')
+  end
 end
