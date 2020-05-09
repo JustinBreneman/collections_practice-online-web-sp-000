b1d4b707081151379f8c39cc5c3081240664a93b
Justin Breneman <justin.breneman@yahoo.com>
2020-05-09 01:40:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,6 +10,6 @@ def sort_array_char_count(array)
   array.sort { |a, b| a.length <=> b.length }
 end
 
-def swap_elements(array)
-  array.sort
+def swap_elements_from_to(array,index,destination_index)
+  
 end
