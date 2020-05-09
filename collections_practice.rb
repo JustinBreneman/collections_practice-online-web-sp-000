47ffbeda252e83b08a0819d55a7027c2e09ee295
Justin Breneman <justin.breneman@yahoo.com>
2020-05-09 02:23:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,3 +14,7 @@ def swap_elements_from_to(array,index,destination_index)
   array[index], array[destination_index] = array[destination_index], array[index]
   array
 end
+
+def swap_elements(array)
+  swap_elements_from_to(array,1,2)
+end
