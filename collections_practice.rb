ceed84cc439d26e39412bad8452a22231a4315d3
Justin Breneman <justin.breneman@yahoo.com>
2020-05-09 03:01:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -54,10 +54,11 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |word, index|
-    if index != 1
-      word << "s"
-    else
-      word
-    end
+    index != 1 ? word << "s" : word
+#    if index != 1
+#      word << "s"
+#    else
+#      word
+#    end
   end
 end
